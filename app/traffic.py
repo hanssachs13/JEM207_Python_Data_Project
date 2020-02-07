f5737cc8124e82d9bc2ee297b6c706c540fbede3
Jan Liehman <jan.liehman@gmail.com>
2020-02-07 21:38:30 +0100

    traffic processing json

--- app/traffic.py
+++ app/traffic.py
@@ -9,6 +9,7 @@ class Traffic:
         self.traffic_url = 'https://raw.githubusercontent.com/datastory/dpp-prepravni-pruzkumy/master/data_csv/TRAM2014_records.csv'
         self.stops_url = 'http://data.pid.cz/stops/json/stops.json'
         self.traffic_csv_path = 'data/traffic.csv'
+        self.traffic_ext_csv_path = 'data/traffic_ext.csv'
         self.traffic_json_path = 'data/traffic.json'
         self.stops_json_path = 'data/stops_pid.json'
         self.final_output_path = 'data/final_traffic.json'
@@ -31,22 +32,31 @@ class Traffic:
             f.write(response.content)
 
     def process_traffic(self):
-        df_traf = pd.read_csv(self.traffic_csv_path)
-        # add StopID column
-        stop_id = df_traf['Číslo uzlu zastávky'].astype(str) + "/" + df_traf['Číslo sloupku zastávky'].astype(str)
-        df_traf['StopID'] = stop_id
-        # add Flow column as sum of passengers entering and exiting
-        df_traf['Flow'] = df_traf.apply(lambda x: x['Nástupy1'] + x['Nástupy2'] + x['Výstupy1'] +x['Výstupy2'], axis = 1)
-        # add Load column as average of passengers on board before arrival and after departure
-        df_traf['Load'] = df_traf.apply(lambda x: (x['PočetPoNástupu1'] + x['PočetPoNástupu2'] + x['PočetPředVýstupem1'] + x['PočetPředVýstupem2'])/2, axis = 1)
-        # convert departure data to datetime
-        df_traf['Skutečný odjezd'] = pd.to_datetime(df_traf['Skutečný odjezd'], infer_datetime_format = True)
-        df_traf['Plánovaný odjezd'] = pd.to_datetime(df_traf['Plánovaný odjezd'], infer_datetime_format = True)
-        df_traf['Hour'] = pd.DatetimeIndex(df_traf['Plánovaný odjezd']).hour
-        # add Delay columns in seconds
-        df_traf['Delay'] = df_traf.apply(lambda x: max((x['Skutečný odjezd'] - x['Plánovaný odjezd'])/np.timedelta64(1,'s'), 0), axis = 1)
+        # create new list
+        traf_list = []
+        # process csv data by line
+        with open(self.traffic_csv_path, 'r', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            # skip header
+            next(reader, None)
+            # for every line
+            for row in reader:
+                # create new item in list
+                traf_list.append({
+                    "id": row[14] + "/" + row[15],
+                    "realTime": row[2],
+                    "schTime": row[3],
+                    "entry": int(row[6]) + int(row[7]),
+                    "exit": int(row[12]) + int(row[13]),
+                    "befArr": int(row[10]) + int(row[11]),
+                    "aftDep": int(row[8]) + int(row[9]),
+                })
 
-        print(df_traf)
+        # sort by id
+        traf_list.sort(key = lambda x: x["id"], reverse = False)
+        # store in file
+        with open("data/traffic_out.json", "w", encoding = 'utf-8') as f:
+            json.dump(traf_list, f, ensure_ascii = False, indent = 4)
 
 
 
