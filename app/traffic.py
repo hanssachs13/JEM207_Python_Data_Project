fd321b2108c1fa538428af09c0cd1836ffff35f1
Jan Liehman <jan.liehman@gmail.com>
2020-02-08 21:24:15 +0100

    added delay, flow, and load calculations

--- app/traffic.py
+++ app/traffic.py
@@ -2,8 +2,11 @@ import requests
 import csv
 import json
 import pandas as pd
+import datetime as dt
 import numpy as np
 import sys
+
+pd.set_option('display.expand_frame_repr', False)
 
 class Traffic:
     def __init__(self):
@@ -82,13 +85,53 @@ class Traffic:
         pd_stops = pd.read_json("data/stops_out.json", encoding = 'utf-8')
         pd_traffic = pd.read_json("data/traffic_out.json", encoding = 'utf-8')
         ST_out = pd.merge(pd_stops, pd_traffic, how = "inner")
+        self.ST_out = ST_out
+        ST_out.to_pickle('jebladata.pkl')
+        # with open(self.final_output_path, "w", encoding = 'utf-8') as f:
+        #    json.dump(ST_out, f, ensure_ascii = False, indent = 4)
         print(ST_out)
-        # ST_out.to_csv("data/stops_traf_merge.csv")
+        # ST_out.to_json("data/stops_traf_merge.json")
+
+    @staticmethod
+    def vystup(data: pd.DataFrame, start: int, end: int, out: str):
+        data = data.copy()
+        data = data.loc[data['realTime'] != '1.1.1900',:]
+        data['schTime'] = pd.to_datetime(data['schTime'], infer_datetime_format = True)
+        data['realTime'] = pd.to_datetime(data['realTime'], infer_datetime_format = True)
+        data['schHour'] = pd.DatetimeIndex(data['schTime']).hour
+        # data_filtered = data.filter((data['schHour'] >= start) & (data['schHour'] < end), axis = 1).copy()
+        data_filtered = data[(data['schHour'] >= start) & (data['schHour'] < end)].copy()
+        # data_filtered[['schTime','realTime']].fillna(0, inplace = True)
+        if out == "delay":
+            data_filtered['output'] = (data_filtered['realTime'] - data_filtered['schTime']).dt.total_seconds()
+            data_filtered.loc[data_filtered['output'] < 0, 'output'] = 0
+            # data_filtered['output'] = data_filtered.apply(lambda x: max((x['realTime'] - x['schTime'])/np.timedelta64(1,'s'), 0), axis = 1)
+            # data_filtered['Delay'] = max((data_filtered['realTime'] - data_filtered['schTime'])/np.timedelta64(1,'s'), 0)
+            Means = data_filtered[['id','name','output', 'lat', 'lon']].groupby(['id','name', 'lat', 'lon']).mean()
+            Means.reset_index([1, 2, 3], inplace = True)
+            # Delay_mns.rename(columns={'Delay':'output'}, inplace = True)
+            return(Means)
+        if out == 'flow':
+            data_filtered['output'] = data_filtered['entry'] + data_filtered['exit']
+            Means = data_filtered[['id', 'name', 'output', 'lat', 'lon']].groupby(['id', 'name', 'lat', 'lon']).mean()
+            Means.reset_index([1, 2, 3], inplace = True)
+            # Flow_mns.rename(columns={'Flow':'output'}, inplace = True)
+            return(Means)
+        if out == 'load':
+            data_filtered['output'] = (data_filtered['aftDep'] + data_filtered['befArr']) / 2
+            Means = data_filtered[['id', 'name', 'output', 'lat', 'lon']].groupby(['id', 'name', 'lat', 'lon']).mean()
+            Means.reset_index([1, 2, 3], inplace = True)
+            # Load_mns.rename(columns={'Load':'output'}, inplace = True)
+            return(Means)
+        else:
+            raise ValueError('Invalid request')
+
 
 
 if __name__ == '__main__':
     traffic = Traffic()
     # traffic.download_data()
-    # traffic.process_traffic()
+    traffic.process_traffic()
     traffic.process_stops()
     traffic.merge_panda()
+    # traffic.vystup(1, 13, "load")
